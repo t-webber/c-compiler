8c7317a50e2c10c363ec993fa5012e93b4ea707d
Bob Bob <bobbob75105@gmail.com>
2023-10-21 15:14:56 +0200

    Part I on my laptop
    Co-authored-by: Rémy Fayet <rems64@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use std::collections::HashSet;
+
+fn preprocess(content: String, _hashmap: &mut HashSet<String>) -> String {
+    content.lines().map(|line| {
+        let line_string= line.to_string();
+        line_string+"\n"
+    }).collect()
+}
+
+fn preprocess_unit(content: String) -> String {
+    let mut includes = HashSet::<String>::new();
+    preprocess(content, &mut includes)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut content: String = String::new();
+    File::open("fichier.c")
+        .expect("Failed to read fichier.c")
+        .read_to_string(&mut content)
+        .unwrap();
+    let preprocessed_file: String = preprocess_unit(content);
+
+    println!("{preprocessed_file}");
 }
