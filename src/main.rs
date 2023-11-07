06911b532ce23841d628af27dfc272a2df750706
Remy Fayet <rems64des@gmail.com>
2023-11-07 16:37:32 +0100

    Refactor code to use preprocessor parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::{fs, path::PathBuf};
+// use std::{fs, path::PathBuf};
 
-use parser::parse;
+use parser::parse_preprocessor;
 
 // mod expression;
 mod parser;
 // mod preprocessor;
 
 fn main() {
-    let input = fs::read_to_string(PathBuf::from("fichier.c")).unwrap();
-    let parsed = parse(input);
+    // let input = fs::read_to_string(PathBuf::from("fichier.c")).unwrap();
+    let input = String::from("a$b");
+    let parsed = parse_preprocessor(&input);
     println!("{parsed:?}");
     // let preprocessed_file = preprocessor::preprocess_unit(PathBuf::from("fichier.c"));
     // println!("{preprocessed_file}");
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,10 @@
-use std::fmt::Debug;
+#![allow(unused)]
+use std::{
+    fmt::Debug,
+    ops::{AddAssign, MulAssign, SubAssign},
+};
 
+#[derive(Debug)]
 pub enum Keyword {
     Auto,
     Double,
@@ -32,22 +37,27 @@ pub enum Keyword {
     Do,
     If,
     Static,
-    While
+    While,
 }
 
+#[derive(Debug)]
 pub enum Operator {
+    // Unary
     Plus,
     Minus,
+    Not,
+    Eval,
+    Subscript,
+
+    // Binary
+    Add,
+    Sub,
     Mul,
     Div,
-    Modulo,
-    Not,
+    Mod,
     BitwiseAnd,
     BitwiseOr,
     BitwiseXor,
-    Negate,
-    
-        
     And,
     Or,
     Xor,
@@ -55,18 +65,27 @@ pub enum Operator {
     ShiftRight,
     Increment,
     Decrement,
-    ShiftLeftAssign,
-    ShiftRightAssign,
     NotEqual,
     Eequal,
+    LessThan,
+    GreaterThan,
+    LessEqual,
+    GreaterEqual,
     AddAssign,
     SubAssign,
     MulAssign,
     DivAssign,
-    ModuloAssign,
-    BitwiseAndAssign,
+    ModAssign,
+    OrAssign,
+    AndAssign,
+    XorAssign,
+    Arrow,
+
+    ShiftLeftAssign,
+    ShiftRightAssign,
 }
 
+#[derive(Debug)]
 pub enum Token {
     Keyword(Keyword),
     Operator(Operator),
@@ -76,27 +95,101 @@ pub enum Token {
     SpecialSymbol,
 }
 
-impl Debug for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
+#[derive(Debug)]
+pub enum PreprocessorToken {
+    DefinedOperator,
+    Operator(Operator),
+    LiteralString(String),
+    LiteralNumber(f32),
+    Macro(String),
 }
 
-impl From<char> for Token {
-    fn from(value: char) -> Self {
-        Token(String::from(value))
+fn token_from_str(token_str: &str) -> Option<PreprocessorToken> {
+    if token_str.is_empty() {
+        return None;
     }
+    let token = if let Ok(number) = token_str.parse::<f32>() {
+        PreprocessorToken::LiteralNumber(number)
+    } else {
+        match token_str {
+            "!" => PreprocessorToken::Operator(Operator::Not),
+            "+" => PreprocessorToken::Operator(Operator::Add),
+            "-" => PreprocessorToken::Operator(Operator::Sub),
+            "*" => PreprocessorToken::Operator(Operator::Mul),
+            "/" => PreprocessorToken::Operator(Operator::Div),
+            "%" => PreprocessorToken::Operator(Operator::Mod),
+            "&" => PreprocessorToken::Operator(Operator::BitwiseAnd),
+            "|" => PreprocessorToken::Operator(Operator::BitwiseOr),
+            "^" => PreprocessorToken::Operator(Operator::BitwiseXor),
+            "<" => PreprocessorToken::Operator(Operator::LessThan),
+            ">" => PreprocessorToken::Operator(Operator::GreaterThan),
+            "++" => PreprocessorToken::Operator(Operator::Increment),
+            "--" => PreprocessorToken::Operator(Operator::Decrement),
+            ">>" => PreprocessorToken::Operator(Operator::ShiftRight),
+            "<<" => PreprocessorToken::Operator(Operator::ShiftLeft),
+            "!=" => PreprocessorToken::Operator(Operator::NotEqual),
+            "==" => PreprocessorToken::Operator(Operator::Eequal),
+            "+=" => PreprocessorToken::Operator(Operator::AddAssign),
+            "-=" => PreprocessorToken::Operator(Operator::SubAssign),
+            "*=" => PreprocessorToken::Operator(Operator::MulAssign),
+            "/=" => PreprocessorToken::Operator(Operator::DivAssign),
+            "%=" => PreprocessorToken::Operator(Operator::ModAssign),
+            "<=" => PreprocessorToken::Operator(Operator::LessEqual),
+            ">=" => PreprocessorToken::Operator(Operator::GreaterEqual),
+            "&=" => PreprocessorToken::Operator(Operator::AndAssign),
+            "|=" => PreprocessorToken::Operator(Operator::OrAssign),
+            "^=" => PreprocessorToken::Operator(Operator::XorAssign),
+            "&&" => PreprocessorToken::Operator(Operator::And),
+            "||" => PreprocessorToken::Operator(Operator::Or),
+            "()" => PreprocessorToken::Operator(Operator::Eval),
+            "[]" => PreprocessorToken::Operator(Operator::Subscript),
+            "->" => PreprocessorToken::Operator(Operator::Arrow),
+            ">>=" => PreprocessorToken::Operator(Operator::ShiftRightAssign),
+            "<<=" => PreprocessorToken::Operator(Operator::ShiftLeftAssign),
+
+            "defined" => PreprocessorToken::DefinedOperator,
+            token => {
+                if token.starts_with('\"') && token.ends_with('\"')
+                    || token.starts_with('\'') && token.ends_with('\'')
+                {
+                    PreprocessorToken::LiteralString(
+                        token
+                            .get(1..token.len() - 1)
+                            .expect("Catastrophic failure")
+                            .to_string(),
+                    )
+                } else {
+                    PreprocessorToken::Macro(token_str.to_string())
+                }
+            }
+        }
+    };
+    Some(token)
 }
 
 #[rustfmt::skip]
-pub fn parse(string: String) -> Vec<Token> {
-    let mut tokens: Vec<Token> = vec![Token::default()];
+pub fn parse_preprocessor(string: &str) -> Vec<PreprocessorToken> {
+    let mut tokens: Vec<PreprocessorToken> = vec![];
+    let mut current_token: String = String::new();
     string.chars().for_each(|c| match c {
-        'A'..='z' | '0'..='9' => tokens.last_mut().unwrap().0.push(c),
-        ' ' => if !tokens.last().unwrap().0.is_empty() { tokens.push(Token::default()) },
-        _ if tokens.last().unwrap().0.is_empty() => { tokens.last_mut().unwrap().0.push(c); tokens.push(Token::default()) }
-        _  => { tokens.push(Token::from(c)); tokens.push(Token::default()) }
+        _ if c.is_alphabetic() || c.is_alphanumeric() => current_token.push(c),
+        ' ' => {
+            if let Some(token) = token_from_str(&current_token) {
+                tokens.push(token);
+                current_token.clear();
+            }
+        }
+        _ => {
+            current_token.push(c);
+            if let Some(token) = token_from_str(&current_token) {
+                tokens.push(token);
+            };
+            current_token.clear();
+        }
     });
+    if let Some(token) = token_from_str(&current_token) {
+        tokens.push(token);
+    }
     tokens
 }
 
